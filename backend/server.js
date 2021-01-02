2c7e7cae35c6062923e8c3c6c92e0350e5e41cf8
Kyle Spencer <kylecspencer@gmail.com>
2021-01-02 18:17:19 -0500

    add more server socket handling

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,16 @@ class Player {
     constructor(identifier, ws) {
         this.identifier = identifier;
         this.ws = ws;
+        ws.on('message', this.handleRecv);
+        ws.on('close', this.handleClose);
+    }
+
+    handleClose() {
+        console.log('player ' + this.identifier + ' disconnected!');
+    }
+
+    handleRecv(data) {
+        console.log('player ' + this.identifier + ' got data ' + data);
     }
 
     socketSend(data) {
