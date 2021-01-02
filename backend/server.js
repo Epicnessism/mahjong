2b6c789af3e0c16760250eb4251cd672d38e369b
Kyle Spencer <kylecspencer@gmail.com>
2021-01-02 01:54:40 -0500

    add websockets to frontend and backend, with scuffed matchmaking system

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+  port: 8888
+});
+
+wss.on('connection', handleNewConnection);
+
+//nobody actually knows if lists are threadsafe in node
+waitingPlayers = [];
+games = [];
+
+function handleNewConnection(ws) {
+    console.log('Got new connection!');
+    randomId = Math.floor(Math.random() * 100);
+    newPlayer = new Player(randomId, ws);
+    newPlayer.socketSend('Welcome to the Mahjong server!');
+    waitingPlayers.push(newPlayer);
+
+    if(waitingPlayers.length >= 4) {
+        newGame = new MahjongGame(waitingPlayers);
+        games.push(newGame);
+        newGame.start();
+
+        //todo this lobby system is super hacky lmao
+        waitingPlayers = [];
+    } else {
+        newPlayer.socketSend("Added to game queue, waiting for more players...");
+    }
+}
+
+class Player {
+    constructor(identifier, ws) {
+        this.identifier = identifier;
+        this.ws = ws;
+    }
+
+    socketSend(data) {
+        this.ws.send(data)
+    }
+}
+
+class MahjongGame {
+    constructor(players) {
+        this.players = players;
+    }
+
+    start() {
+        console.log("New game starting...")
+        this.players.forEach(player => player.socketSend("Game Starting..."));
+    } 
+}
