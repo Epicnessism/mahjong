87e6ab6418f4eb39e77940bd516b7022ee7a7909
Tony Wang <tonywang0697@yahoo.com>
2021-01-03 14:10:30 -0500

    added correct tiles for mahjong

--- backend/src/mahjong.js
+++ backend/src/mahjong.js
@@ -2,26 +2,34 @@ const util = require('./util')
 
 //referencing this https://en.wikipedia.org/wiki/Mahjong_tiles
 //TODO add the rest
-const dotTiles = Array.from(Array(9).keys()).map((x) => x + 1).map((x) => "dots_" + x);
-const bambooTiles = Array.from(Array(9).keys()).map((x) => x + 1).map((x) => "bamboo_" + x);
-const characterTiles = Array.from(Array(9).keys()).map((x) => x + 1).map((x) => "char_" + x);
+const dotTiles = Array.from(Array(9).keys()).map((x) => x + 1).map((x) => "dot_" + x);  //x4
+const bambooTiles = Array.from(Array(9).keys()).map((x) => x + 1).map((x) => "bamboo_" + x); //x4
+const tenkTiles = Array.from(Array(9).keys()).map((x) => x + 1).map((x) => "tenk_" + x); //x4
+const characterTiles = Array.from(Array(7).keys()).map((x) => x + 1).map((x) => "char_" + x); //x4
+const flowerTiles = Array.from(Array(4).keys()).map((x) => x + 1).map((x) => "flower_" + x); //x2 //separated the flowers since flowers are only used in certain sets of mahjong not all.
 
-const tileSet = dotTiles.concat(bambooTiles).concat(characterTiles);
+const tileSuitSetUnique = dotTiles.concat(bambooTiles).concat(tenkTiles).concat(characterTiles);
+const tileSetFullnoFlowers = tileSetUnique.concat(tileSetUnique).concat(tileSetUnique).concat(tileSetUnique);
+const tileSetFullwFlowers = tileSetUnique.concat(tileSetUnique).concat(tileSetUnique).concat(tileSetUnique).concat(flowerTiles).concat(flowerTiles);
 
 class MahjongGame {
-    constructor(players) {
+    constructor(players, gameType) {
         this.players = players;
-        this.tiles = Array.from(tileSet);
+        if(gameType == "flowers") {
+            this.tiles = Array.from(tileSetFullwFlowers);
+        } else {
+            this.tiles = Array.from(tileSetFullnoFlowers);
+        }
         util.shuffleArray(this.tiles);
         this.tileFrontIdx = 0;
-        this.tileBackIdx = this.tiles.length - 1
+        this.tileBackIdx = this.tiles.length - 1;
     }
 
     get tilesLeft() {
         return this.tileBackIdx - this.tileFrontIdx + 1;
     }
 
-    takeTiles(count, useBack = false) {
+    takeTile(count, useBack = false) {
         //there are edge cases here
         var tileSubset = [];
 
