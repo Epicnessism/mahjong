d1796ae546e13808ac24435184ffa4a825df5311
Tony Wang <tonywang0697@yahoo.com>
2021-04-01 19:44:28 -0400

    got vue working, still leanring, started creating login functionality

--- vue-mahjong/src/views/Home.vue
+++ vue-mahjong/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <Login v-if="loggedIn" msg="Please Login!!!!!" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <v-btn @click="toggleLogin()">
+      Toggle Login view
+    </v-btn>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Login from "@/components/Login.vue"
 
 @Options({
   components: {
     HelloWorld,
+    Login,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  loggedIn = false;
+
+  toggleLogin() {
+    this.loggedIn = !this.loggedIn
+  }
+}
 </script>
